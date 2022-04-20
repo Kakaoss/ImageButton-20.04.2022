9106eed5fdd45a64899c92614b4c3ba8fc423907
piasek <piaskowski3456@gmail.com>
2022-04-20 08:20:46 +0200

    koniec

--- app/src/main/java/com/example/imagebutton_20042022/MainActivity.kt
+++ app/src/main/java/com/example/imagebutton_20042022/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.imagebutton_20042022
 
+import android.content.Context
 import android.graphics.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ToggleButton
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,9 +34,17 @@ class MainActivity : AppCompatActivity() {
             canvas.drawBitmap(bitmapCzerwony, null , RectF(0f , 0f, bitmap.width.toFloat(), bitmap.height.toFloat()) , paint)
             canvas.drawBitmap(bitmap, 0f ,0f, paint)
             mojprzycisk.setImageBitmap(filterbitmap)
+        }
 
-
-
+        kolorGreen.setOnClickListener {
+            if(kolorGreen.isChecked){
+                mojprzycisk.setColorFilter(R.color.green)
+                kolorGreen.background = (ContextCompat.getDrawable(kolorGreen.context, R.color.green))
+            }
+            else {
+                mojprzycisk.setColorFilter(null)
+                kolorGreen.background = (ContextCompat.getDrawable(kolorGreen.context, R.color.darkgreen))
+            }
         }
     }
 }
